b25332048e35441b82e6d4f36987c158d9d18459
Horgito Mhillaj <gito@Horgitos-MacBook-Pro.local>
2024-05-26 13:48:21 +0200

    update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+
+function Footer() {
+  return (
+    
+
+<footer class="p-4 bg-black md:p-8 lg:p-10">
+  <div class="mx-auto max-w-screen-xl text-center">
+      <div  class="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white">
+
+        Selimi Travel  
+      </div>
+      <p class="my-6 text-gray-500 dark:text-gray-400">"Filloni udhëtimet të paharrueshme me Selimi Travel, ku çdo destinacion është një kapitull i ri në aventurën tuaj."</p>
+      <ul class="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
+          <li>
+              <a href="link" class="mr-4 hover:underline md:mr-6 ">Instagram</a>
+          </li>
+          <li>
+              <a href="link" class="mr-4 hover:underline md:mr-6">Tiktok</a>
+          </li>
+          <li>
+              <a href="link" class="mr-4 hover:underline md:mr-6 ">Facebook</a>
+          </li>
+          <li>
+              <a href="link" class="mr-4 hover:underline md:mr-6">Google Maps</a>
+          </li>
+          <li>
+              <a href="link" class="mr-4 hover:underline md:mr-6">Call</a>
+          </li>
+        
+      </ul>
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <div className='bold'>Selimi Travel™</div> All Rights Reserved.</span>
+  </div>
+</footer>
+
+
+
+  )
+}
+
+export default Footer
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,11 +17,11 @@ const Navbar = () => {
         {/* Desktop Menu */}
         <div className='hidden md:flex'>
           <ul className='flex text-white items-center'>
-            <li className="mr-4">Kreu</li>
-            <li className="mr-4">Rreth Nesh</li>
-            <li className="mr-4">Sherbimet</li>
-            <li className="mr-4">Kontakt</li>
-            <button type="button" className="text-black bg-white hover:bg-slate-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Telefononi</button>
+            <li className="mr-4 hover:text-gray-400">Kreu</li>
+            <li className="mr-4 hover:text-gray-400">Rreth Nesh</li>
+            <li className="mr-4 hover:text-gray-400">Sherbimet</li>
+            <li className="mr-4 hover:text-gray-400">Kontakt</li>
+            <button type="button" className="text-black hover:text-white  bg-white hover:bg-black font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Telefononi</button>
           </ul>
         </div>
 
@@ -31,14 +31,14 @@ const Navbar = () => {
         </div>
 
         {/* Mobile Menu */}
-        <div className={nav ? 'w-full bg-black text-white absolute top-[90px] left-0 flex justify-center text-center ' : 
-    'hidden md:hidden'}>
+        <div className={nav ? 'w-full bg-black text-white absolute top-[90px] left-0 flex justify-center text-center ' :  'hidden md:hidden'}>
+          
           <ul className='pb-5'>
-            <li className='text-2xl pb-4'>Kreu</li>
-            <li className='text-2xl pb-4'>Rreth Nesh</li>
-            <li className='text-2xl pb-4'>Sherbimet</li>
-            <li className='text-2xl pb-4'>Kontakti</li>
-            <button type="button" className="text-black hover:border-blue-200 border-2 bg-white hover:bg-slate-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Telefononi</button>
+            <li className='text-2xl pb-4 hover:text-gray-400'>Kreu</li>
+            <li className='text-2xl pb-4 hover:text-gray-400'>Rreth Nesh</li>
+            <li className='text-2xl pb-4 hover:text-gray-400'>Sherbimet</li>
+            <li className='text-2xl pb-4 hover:text-gray-400'>Kontakti</li>
+            <button type="button" className="text-black hover:border-blue-200 border-2 bg-white hover:bg-gray-400 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Telefononi</button>
           </ul>
         </div>
       </div>
